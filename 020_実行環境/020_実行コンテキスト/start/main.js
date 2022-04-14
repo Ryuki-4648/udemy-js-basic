1037a7ebb420005eac4774ed4a23d9a7c163a8d2
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-04-15 07:47:56 +0900

    コールスタック　メモ

--- 020_実行環境/020_実行コンテキスト/start/main.js
+++ 020_実行環境/020_実行コンテキスト/start/main.js
@@ -33,4 +33,3 @@ const b =() => {
 
 //console.log(a);
 b();
-ba();
--- 020_実行環境/030_コールスタック/start/main.js
+++ 020_実行環境/030_コールスタック/start/main.js
@@ -1,9 +1,27 @@
 function a() {
+  console.log('function a');
 }
 function b() {
-    a();
+  a();
+  console.log('function b');
 }
 function c() {
-    b();
+  b();
+  console.log('function c');
 }
 c();
+
+/**
+ * コールスタック
+ * a
+ * b
+ * c
+ * グローバル
+ * 
+ * と言うふうに積まれていく
+ */
+
+/**
+ * 後から積み重なったものが一番最初に消滅するスタックの仕組み
+ * 後入れ先だし　LIFO Last In, First Outという。
+ */
