8d415d8f6ac9cbd70ab5665fa44500be107e7b82
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-04-19 06:19:45 +0900

    ホイスティングの追加メモ

--- 020_実行環境/040_ホイスティング/start/main.js
+++ 020_実行環境/040_ホイスティング/start/main.js
@@ -4,13 +4,15 @@
  * コンテキスト内で宣言した変数や関数の定義を
  * コード実行前にメモリーに配置すること
  * 
- * 
+ * ホイスティングはコンテキストが生成されるたびに起こる
+ * グローバルコンテキストが生成されるタイミングで宣言部のメモリスペースが確保される
  */
 
 
 func(); // 関数の実行1　コンソールに表示される
+// ↑　★この関数funcの実行前に、すでに関数がメモリ上に存在している
 
-// 関数の定義　関数の実行1よりも前にメモリ上に配置されている
+// 関数宣言　関数の定義　関数の実行1よりも前にメモリ上に配置されている
 function func() {
   console.log('func is called');
 }
@@ -27,9 +29,9 @@ a();
 
 function a() {
   console.log(c);
-  var c = 1;
+  var c = 1; // undefinedが返ってくる。letの場合は「cがまだ初期化されていない」(uninitialized)というエラーが出る
 
-  d();
+  d(); // 宣言部より前に配置しても、既にメモリ上に存在するのでd is calledが出力される
   function d(){
     console.log('d is called');
   }
@@ -42,7 +44,9 @@ a();
 
 
 
-
+/**
+ * varの場合
+*/
 console.log(b); // undefined 値が未定義
 var b = 0;
 /*
@@ -53,18 +57,21 @@ console.log(b); // undefinedが出力される
 var b = 0;
 console.log(b); // 値0が取得される
 
-宣言のキーワードを見つけたら、メモリにスペースを確保して、undefinedをいう値を設定している
+★★ 宣言のキーワードを見つけたら、メモリにスペースを確保して、undefinedをいう値を設定している
+★★ ホイスティングの初期値としてundefinedが設定される
 */
 
 
 
 
 
-
-// console.log(c); // Cannot access 'c' before initialization cが初期化されていないというエラー
+/**
+ * letの場合
+*/
+// console.log(c); とすると、Cannot access 'c' before initialization cが初期化されていないというエラーが出る
 let c = 0;
 /*
-let(const)を使った場合、undefinedの初期化がJavaScriptエンジンによって行われない
+★★ let(const)を使った場合、undefinedの初期化がJavaScriptエンジンによって行われない
 */
 
 
@@ -74,11 +81,11 @@ let(const)を使った場合、undefinedの初期化がJavaScriptエンジンに
 // 関数の定義方法 関数式
 
 // c(); 宣言部より上で実行しようとするとエラーになる TypeError: c is not a function
+// 変数を宣言し、無名関数を代入する方法（関数式）
+
+//console.log(e); // ←宣言部より前にconsole.log(e);とすると「Cannot access 'e' before initialization」のエラーが表示される
 const e = function(){
   console.log('e is called');
 }
 e();
 
-
-
-// 関数宣言と関数式
