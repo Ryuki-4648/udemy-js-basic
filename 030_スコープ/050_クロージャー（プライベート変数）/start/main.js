d5ca094c9dce973bb080a887b025ce0fa429755c
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-04-22 06:49:33 +0900

    クロージャー　プライベート変数

--- 030_スコープ/050_クロージャー（プライベート変数）/start/main.js
+++ 030_スコープ/050_クロージャー（プライベート変数）/start/main.js
@@ -34,6 +34,69 @@ fn1();
 */
 
 
+// カウントアップする関数を作成したい(1)
+increment();
+increment();
+increment();
+
+function increment(){
+  let num = 0;
+  num += 1;
+  console.log(num);
+}
+
+// これだと「１」が３回出力されてしまう
+// 関数の中で変数numを宣言してしまっているので、numは０になる
+// 関数がよばれるたびに初期化するのではなく、グローバルテキストが実行されるタイミングで初期化を行えば良い。
 
 
+// カウントアップする関数を作成したい(2)
+let number = 0;
+incrementFunc();
+incrementFunc();
+incrementFunc();
 
+function incrementFunc() {
+  number += 1;
+  console.log(number); // 1 2 3
+}
+
+
+/**
+ * numberがグローバルテキストのため、関数を通さずにどこからでもアクセス可能になる
+ * 　↓　解決方法
+ * プライベート変数という、関数の内部に変数を持ちながら、外部からはアクセスできないような仕組みをクロージャーを使って実装する
+*/
+
+
+console.log('---------- クロージャーを用いる ----------');
+
+
+// クロージャーを用いる
+function incrementFactory() {
+
+  let number = 0; // incrementFactoryの関数スコープ内に収まっているので、外部からはアクセス不可
+
+  function increment02() {
+    number += 1;
+    console.log(number);
+  }
+
+  return increment02; // 関数の返却
+}
+
+const increment02 = incrementFactory(); // numberの宣言はここだけ
+// incrementFactoryを実行すると、numberの宣言が行われ、increment02の宣言が行われ、returnでincrement02が返ってくる
+// const increment02には返却された関数increment02が入っている
+
+increment02(); // 返却された関数increment02を実行している。numberの初期化は行われていない。
+increment02();
+increment02();
+increment02();
+increment02();
+
+/**
+ * incrementFactory 「Factory」はなにかを生成するような関数につけることが多い
+ * incrementFactoryの関数スコープで「number」が存在する。
+ * 内側にある関数incrementFuncからは、その変数numberが参照できる。
+ */
