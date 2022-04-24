ac2ae555fa2f05f39a0f842631505e2ef75990b7
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-04-24 14:02:02 +0900

    スコープの練習問題　自分の回答

--- 030_スコープ/910_修了確認_スコープ/start/main.js
+++ 030_スコープ/910_修了確認_スコープ/start/main.js
@@ -9,8 +9,9 @@
 function fn() {
     if(true) {
         let a = 'fn called';
+        return a;
     }
-    return a; // ReferenceError: a is not defined
+    //return a; // ReferenceError: a is not defined
 }
 
 const result = fn();
@@ -21,12 +22,13 @@ console.log(result);
  * fn2内の記述を変更して、各コンソールで
  * 期待値を出力するように修正してください。
  */
-var val = 'val1';
+//var val = 'val1';
 function fn2() {
+    var val = 'val1';
     console.log(val); // 期待値->'val1'
 
     if(true) {
-        var val = 'val2';
+        let val = 'val2'; // letに変更し、ブロックスコープを有効にする
         console.log(val); // 期待値->'val2'
     }
 
@@ -44,5 +46,35 @@ fn2();
  * increment(); // 期待値->2
  * increment(); // 期待値->3
  * increment(); // 期待値->4
+*/
+
+/**
+ * ・プライベート変数の定義
+ * 関数の外部からアクセスできない変数のこと
+ * 
+ * 内側に定義されている関数からレキシカルスコープの変数への参照を保持している場合、
+ * この状態を「クロージャー」と呼ぶ。
+*/
+
+/**
+  function increment(){
+    let number = 0;
+    number += 1;
+    console.log(number);
+  }
+  
+  ↑　これだと「1」が毎回出力される「
+  関数の中で変数numberを宣言している。
+  関数がよばれるたびに初期化するのではなく、「グローバルテキストが実行されるタイミング」で初期化を行えば良い。
  */
 
+
+let number = 0;
+function increment(){
+  number += 1;
+  console.log(number);
+}
+increment();
+increment();
+increment();
+increment();
