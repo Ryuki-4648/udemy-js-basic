af37b6c499f77be3081c83c7dccd86514d95dca0
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-04-26 07:34:15 +0900

    オブジェクトが多階層だった場合

--- 040_変数/090_参照と分割代入/start/main.js
+++ 040_変数/090_参照と分割代入/start/main.js
@@ -38,3 +38,23 @@ function fn({ prop }){ // 特定のプロパティのみ使用したい場合。
   console.log(a, prop); // 0 1
 }
 fn(a);
+
+
+
+
+// オブジェクトが多階層だった場合
+
+const c = {
+  prop1: {
+    prop2: 0
+  }
+}
+
+let { prop1 } = c;
+console.log(prop1); // prop2: 0
+
+prop1.prop2 = 1;
+console.log(c, prop1);
+// cのprop1も1に変更されている
+
+// オブジェクトの参照を保持しているため、分割代入元のオブジェクトにも影響が出る
