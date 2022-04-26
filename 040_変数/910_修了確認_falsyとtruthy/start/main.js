3461915c4f75f85697c217e17a84a24da472bec2
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-04-27 07:38:40 +0900

    回答例

--- 040_変数/910_修了確認_falsyとtruthy/start/main.js
+++ 040_変数/910_修了確認_falsyとtruthy/start/main.js
@@ -13,6 +13,24 @@ console.log('-1 == false', -1 == false); // true, true
 console.log('!10 === false', !10 === false); // false, false
 
 /**
+ * 0 == false true
+ * 0 === false false
+ * "false" == false false 文字列がfalseかどうかの判定になる
+ * "0" == 0' true 暗黙的な型変換がおこる。stringがnumgerに変換される
+ * Boolean("0") === false false booleanによって真偽値に変換される。
+ * 　　　　　　　　　　　　　　文字列の0はtruthyなのでbooleanで変換するとtrueとなる。
+ * 　　　　　　　　　　　　　　true === falseとなり、結果はfalse
+ * Boolean(0) === false true 数値の0であればfalsyなので、booleanで変換するとfalseとなる。
+ * !Boolean(0) === false false boolean(0)はfalseなので、!Boolean(0)はtrueとなる。
+ * 　　　　　　　　　　　　　　　　　true === falseで結果はfalse
+ * -1 == false true 数値でfalseと等価になるのは「0」のみなので、-1はtruthyな値となる。
+ * !10 === false true !10がfalseと厳密に等価になるか？
+ * 　　　　　　　　　　　　!ノット演算子を使うと、booleanの型に変換される。truthyな値10に対し、booleanでtrueからfalseに変換される
+*/
+
+
+
+/**
  * 問題２：
  * 関数fnの引数numが渡ってこない場合（undefinedまたはnullの場合）のみ、
  * 初期値として-1を設定したいものとします。
@@ -23,10 +41,15 @@ console.log('!10 === false', !10 === false); // false, false
 let a = 1;
 
 function fn(num) {
-    num = num && -1;
+  if(num === undefined || num === null) {
+    num = -1;
+  }
     console.log(num);
 }
 fn(a);
+
+// デフォルト値は渡ってきた変数が「未定義」の場合のみ有効になる
+// 未定義はundefined。nullの場合は初期化されない。
 
 
 /**
@@ -41,9 +64,21 @@ fn(a);
 */
 
 
+// 自分の回答
 let greeting = (name, msg = "hello") => {
   console.log(`${msg}, ${name}`);
 }
 
 greeting("Bob", "hi");
 greeting("Bob");
+
+
+// 回答
+let greeting02 = (name02, msg02) => {
+  msg02 = msg02 || 'hello';
+  console.log(`${msg02}, ${name02}`);
+}
+
+greeting02("Bob");
+greeting02("Bob", "hi");
+greeting02("Bob", "");
