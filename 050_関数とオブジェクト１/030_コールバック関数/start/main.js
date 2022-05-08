c9cf5dc006218f101aae992a0919659465fe51bb
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-05-09 06:49:08 +0900

    引数が渡ってくる場合、setTimeoutの使用例

--- 050_関数とオブジェクト１/030_コールバック関数/start/main.js
+++ 050_関数とオブジェクト１/030_コールバック関数/start/main.js
@@ -6,8 +6,8 @@
 */
 
 
-function hello(){
-  console.log('hello');
+function hello(name){
+  console.log('hello, ' + name);
 }
 
 function bye(){
@@ -15,8 +15,19 @@ function bye(){
 }
 
 function fn(cb){
-  cb(); // 実行可能なオブジェクトを実行する
+  cb('Tom'); // 実行可能なオブジェクトを実行する。
 }
 
 fn(hello); // hello関数を引数として渡している。このときに渡している関数を「コールバック関数」という
 fn(bye);
+
+// 無名関数でも可能
+fn(function(name){
+  console.log('hello!!' + name);
+})
+
+
+
+
+// よく使う場合
+setTimeout(hello, 2000); // 2s経ってから表示される
