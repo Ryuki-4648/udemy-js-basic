b0865c6a14cc5dac7f69106432289331cd7ac549
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-05-09 07:00:20 +0900

    this　「呼び出し元のオブジェクト」への参照を保持するキーワード

--- 050_関数とオブジェクト１/040_this/start/main.js
+++ 050_関数とオブジェクト１/040_this/start/main.js
@@ -1,6 +1,20 @@
-const person = {
-    name: 'Tom',
-    hello: function() {
+/** 
+ * this 
+ * 
+ * 関数コンテキスト
+ * thisはjavascriptエンジンによって設定され、関数内で使うことができる
+ * 実行コンテキストによって、thisの参照先は異なる。一般的には関数コンテキストによってthisの参照先が変わることが多い。
+ * 
+ * 「呼び出し元のオブジェクト」への参照を保持するキーワードのこと
+* */
 
-    }
+
+// thisの使用例
+const person = {
+  name: 'Tom',
+  hello: function() {
+    console.log('Hello ' + this.name); // thisがオブジェクトのpersonを参照する
+  }
 }
+
+person.hello();
