58ef505c29139411d3531d565a02ee93ea8dbf73
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-05-09 07:24:11 +0900

    参照のコピーとthis。関数の中でthisを呼んだ場合：ウィンドウオブジェクトを参照する

--- 050_関数とオブジェクト１/050_参照のコピーとthis/start/main.js
+++ 050_関数とオブジェクト１/050_参照のコピーとthis/start/main.js
@@ -1,7 +1,51 @@
+window.name = 'John';
+
 const person = {
     name: 'Tom',
     hello: function() {
         console.log('Hello ' + this.name);
+        a(); // Hello Johnとなる
+        const person = {
+          name: 'Tim',
+          hello: function() {
+              console.log('Hello ' + this.name); // オブジェクトのメソッド：呼び出し元のオブジェクトが取得される
+              a(); // Hello Johnとなる。関数として実行：ウィンドウオブジェクトが取得される
+          }
+        }
+        person.hello();
     }
 }
+
+
+// コピーする場合動きが変わる
+//const ref = person.hello;
+//ref(); // Hello　になる。window.name = 'John';にするとHello Johnと表示される
+
 person.hello();
+
+
+
+/**
+ * 
+ * person
+ * hello →　functinon　→　function(オブジェクトへの参照がコピーされる)
+ * name　→　'Tom'            ↑ref
+ * 
+ * 直接refから関数を呼び出している
+ * 呼び出し元のオブジェクトがpersonではなくなるので、this.nameが取れない
+*/
+
+
+/**
+ * オブジェクトのメソッド　として実行される場合
+ * 'this' => 呼び出し元オブジェクト
+ * 
+ * 関数　として実行される場合
+ * 'this' => グローバルオブジェクト
+*/
+
+
+
+function a (){
+  console.log('Hello ' + this.name); // 関数の中でthisを呼んだ場合：ウィンドウオブジェクトを参照する
+}
