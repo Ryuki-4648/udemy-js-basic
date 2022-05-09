33a55fabce666de9dead4a19bd55803bcab18c47
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-05-09 21:29:32 +0900

    コールバック関数とthis 関数として実行する場合　this -> グローバルオブジェクト

--- 050_関数とオブジェクト１/060_コールバック関数とthis/start/main.js
+++ 050_関数とオブジェクト１/060_コールバック関数とthis/start/main.js
@@ -1,9 +1,24 @@
 window.name = 'John';
 
 const person = {
-    name: 'Tom',
-    hello: function() {
-        console.log('Hello ' + this.name);
-    }
+  name: 'Tom',
+  hello: function() {
+    console.log('Hello ' + this.name);
+  }
 }
 person.hello();
+
+
+
+
+// person.hello()をコールバック関数として、他の関数の引数に与えた場合どうなるか？
+
+function fn(ref) {
+  ref(); // 関数として実行されている this -> グローバルオブジェクト
+}
+// personのhelloプロパティが参照している先の関数がrefに渡される
+
+fn(person.hello); // Hello Johnになる！！
+// メソッドと他の変数に代入しているのと同じ！！！
+
+// オブジェクトのメソッドを引数で渡した段階で関数になる
