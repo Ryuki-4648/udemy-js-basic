d7f99f4d49907389a700c43f287ab36cbbc01121
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-05-13 06:37:19 +0900

    修了確認　クロージャーとコールバック関数　自分の回答

--- 050_関数とオブジェクト１/930_修了確認_クロージャーとコールバック/start/main.js
+++ 050_関数とオブジェクト１/930_修了確認_クロージャーとコールバック/start/main.js
@@ -7,12 +7,12 @@
  * 条件としてはhello関数の引数には必ず
  * nameの引数を取るものとします。
  * 
- */
+*/
 function hello(name) {
-  
+  console.log('hello ' + name);
 }
 
 /**
  * 実行文は以下の通りです。
- */
+*/
 setTimeout(hello('Tom'), 1000);
