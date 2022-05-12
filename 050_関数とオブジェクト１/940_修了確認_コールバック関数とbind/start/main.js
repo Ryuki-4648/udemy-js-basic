c7510644ac5ec8762f6f459f4c92e0ad762a7e13
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-05-13 07:38:35 +0900

    修了確認　コールバックとbind 回答

--- 050_関数とオブジェクト１/940_修了確認_コールバック関数とbind/start/main.js
+++ 050_関数とオブジェクト１/940_修了確認_コールバック関数とbind/start/main.js
@@ -5,28 +5,44 @@
  * ようにcalcFactory関数内の実装を変更してみてください。
  */
 function calcFactory(val, callback) {
-    return {
-        plus: function(target) {
-            const newVal = val + target;
-            callback(`${val} + ${target} = ${newVal}`);
-            val = newVal;
-        },
-        minus: function(target) {
-            const newVal = val - target;
-            callback(`${val} - ${target} = ${newVal}`);
-            val = newVal;
-        },
-        multiply: function(target) {
-            const newVal = val * target;
-            callback(`${val} x ${target} = ${newVal}`);
-            val = newVal;
-        },
-        divide: function(target) {
-            const newVal = val / target;
-            callback(`${val} / ${target} = ${newVal}`);
-            val = newVal;
-        }
-    };
+
+  /**
+   * 以下だとコンソールに何も出力されない
+   * function callbackAfter1s (){
+    setTimeout(callback, 1000);
+  }
+　↑ console.logまたはalertがただ渡ってきているだけになる。setTimeout(console.log, 1000);の状態。
+  callbackAfter1s(`${val} + ${target} = ${newVal}`);
+  */
+
+
+  function callbackAfter1s (str){ // 文字列を受け取る引数str
+    setTimeout(callback.bind(null, str), 1000); // strをcallbackに渡してあげたい
+  }
+
+
+  return {
+    plus: function(target) {
+      const newVal = val + target;
+      callbackAfter1s(`${val} + ${target} = ${newVal}`); // 引数として文字列が渡されている
+      val = newVal;
+    },
+    minus: function(target) {
+      const newVal = val - target;
+      callbackAfter1s(`${val} - ${target} = ${newVal}`);
+      val = newVal;
+    },
+    multiply: function(target) {
+      const newVal = val * target;
+      callbackAfter1s(`${val} x ${target} = ${newVal}`);
+      val = newVal;
+    },
+    divide: function(target) {
+      const newVal = val / target;
+      callbackAfter1s(`${val} / ${target} = ${newVal}`);
+      val = newVal;
+    }
+  };
 }
 
 const calc = calcFactory(10, console.log);
