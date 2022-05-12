9e7d0d4ab141bca79c6cc552856ab59f31a447f3
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-05-13 06:44:28 +0900

    修了確認　クロージャーとコールバック関数　回答

--- 050_関数とオブジェクト１/930_修了確認_クロージャーとコールバック/start/main.js
+++ 050_関数とオブジェクト１/930_修了確認_クロージャーとコールバック/start/main.js
@@ -8,11 +8,35 @@
  * nameの引数を取るものとします。
  * 
 */
+
+// 自分の回答
 function hello(name) {
   console.log('hello ' + name);
 }
+
+// 回答
+function hello(name) {
+  return function(){
+    console.log('hello ' + name);
+  }
+}
+/**
+ * 関数がhelloの実行結果として返却される
+ * 
+ * 結果的に以下の形になる
+ * setTimeout(function(){
+    console.log('hello ' + name);
+  }, 1000);
+*/
+
 
 /**
  * 実行文は以下の通りです。
 */
 setTimeout(hello('Tom'), 1000);
+
+/**
+ * setTimeoutの第一引数に関数を渡す必要がある
+ * この時点で関数helloはすでに実行されてしまっている！！（丸括弧がある時点で関数の実行となる）
+ * →　setTimeoutの第一引数に設定されるのは関数helloの戻り値となる
+*/
