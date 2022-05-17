a487c1f9b700cb4f0a3610adf5fac2a678fafea2
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-05-18 07:03:01 +0900

    修了確認　クロージャーとアロー関数　回答

--- 050_関数とオブジェクト１/950_修了確認_クロージャーとアロー関数/start/main.js
+++ 050_関数とオブジェクト１/950_修了確認_クロージャーとアロー関数/start/main.js
@@ -7,6 +7,20 @@ console.log('---------- 問題1 ----------');
  * 問題１：
  * addNumberFactoryを短く書き直してみましょう。
  */
+
+
+
+// 回答
+const addNumberFactory = num => value => num + value;
+
+const add5 = addNumberFactory(5);
+const result = add5(10);
+console.log(result);
+
+
+/*
+元の形
+
 function addNumberFactory(num) {
     function addNumber(value) {
         return num + value;
@@ -17,6 +31,7 @@ function addNumberFactory(num) {
 const add5 = addNumberFactory(5);
 const result = add5(10);
 console.log(result);
+*/
 
 // 自分の回答
 const addNumberFactory2 = (num) => {
@@ -29,10 +44,56 @@ console.log(result2);
 
 console.log('---------- 問題2 ----------');
 
+
+
+
+
+
+
+
 /**
  * 問題２：
  * incrementFactoryを短く書き直してみましょう。
  */
+
+ // 回答
+const incrementFactory = () => {
+  let num = 0;
+  return () => {
+    num = num + 1;
+    console.log(num);
+  };
+}
+
+const increment = incrementFactory();
+
+increment();
+increment();
+increment();
+increment();
+
+
+
+// 自分の回答
+const incrementFactory2 = () => {
+  let num = 0;
+  let a = () => {
+    num = num + 1;
+    console.log(num);
+  }
+  return a;
+}
+
+const increment2 = incrementFactory2();
+
+increment2();
+increment2();
+increment2();
+increment2();
+
+
+/*
+元の形
 function incrementFactory() {
     
     let num = 0;
@@ -51,20 +112,4 @@ increment();
 increment();
 increment();
 increment();
-
-// 自分の回答
-const incrementFactory2 = () => {
-  let num = 0;
-  let a = () => {
-    num = num + 1;
-    console.log(num);
-  }
-  return a;
-}
-
-const increment2 = incrementFactory2();
-
-increment2();
-increment2();
-increment2();
-increment2();
+*/
