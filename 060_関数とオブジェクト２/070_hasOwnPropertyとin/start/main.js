0ce782d79cb77df85dbcd74b64f77ea40f2aad9e
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-05-23 07:13:22 +0900

    hasOwnPropertyとin　inを使うと、プロトタイプチェーンも遡って調べることができる。

--- 060_関数とオブジェクト２/070_hasOwnPropertyとin/start/main.js
+++ 060_関数とオブジェクト２/070_hasOwnPropertyとin/start/main.js
@@ -1,10 +1,41 @@
+/**
+ * hasOwnPropertyとin
+ * 
+ * hasOwnProperty
+ * 「自分自身」のオブジェクトのプロパティとして、引数で与えられた値が存在するか確認できる
+*/
+
+
+
 function Person(name, age) {
-    this.name = name;
-    this.age = age;
+  this.name = name;
+  this.age = age;
 }
 
 Object.prototype.hello = function() {
-    console.log('Object: hello ' + this.name);
+  console.log('Object: hello ' + this.name);
 }
 
+
 const bob = new Person('Bob', 18);
+
+
+
+const result = bob.hasOwnProperty('name');
+console.log(result); // true
+
+const result2 = bob.hasOwnProperty('hello'); // helloはプロトタイプに格納されているプロパティになる
+console.log(result2); // false
+
+console.log('name' in bob); // true
+console.log('hello' in bob); // true
+console.log('bye' in bob); // false
+console.log('hasOwnProperty' in bob); // true オブジェクトのメソッドとして見つかるため
+
+/**
+ * bobというオブジェクトのプロパティ or　そのオブジェクトが保持してる__proto__に、
+ * 文字列のプロパティ or メソッドが格納されているか調べる。
+ * 
+ * プロトタイプチェーンも遡って調べることができる。
+ * helloもtrueとなる。
+*/
