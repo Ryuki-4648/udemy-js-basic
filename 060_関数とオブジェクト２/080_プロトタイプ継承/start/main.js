98bce58ad7690ac0086c652f723e954b035c274f
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-05-23 07:38:44 +0900

    プロトタイプ継承　別のコンストラクター関数のプロトタイプを受け継ぐことができる

--- 060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
+++ 060_関数とオブジェクト２/080_プロトタイプ継承/start/main.js
@@ -1,3 +1,15 @@
+/**
+ * ◎プロトタイプ継承
+ * 別のコンストラクター関数のプロトタイプを受け継いで、機能を流用できるようにすること。
+ * コンストラクター間で機能の受け渡しが行える。
+ * 
+ * 
+ * ◎継承
+ * 別のコンストラクター関数を受け継ぐこと。
+*/
+
+
+
 function Person(name, age) {
   this.name = name;
   this.age = age;
@@ -6,3 +18,60 @@ function Person(name, age) {
 Person.prototype.hello = function() {
   console.log('hello ' + this.name);
 }
+
+
+function Japanese(name, age, gender){
+  Person.call(this, name, age); // this.nameとthis.ageが設定される
+
+  this.gender = gender; // Japaneseにのみプロパティを追加
+}
+
+Japanese.prototype = Object.create(Person.prototype); // Personのプロトタイプが、Japaneseのプロトタイプの中に含まれるプロトタイプとなる
+/**
+ * コンソールに　Japanese.prototype　を入力　→　Person {}
+ * 
+ * Object.create(Person.prototype);
+ * →　第一引数に渡したオブジェクトをプロパティに持つ、空のオブジェクトを生成（Person {}）
+ * Person {}の中の[[Prototype]]がPersonのプロトタイプとなる
+*/
+
+
+// 上書き。Personのプロトタイプとは別に、独立してJapaneseのプロトタイプが存在する。
+Japanese.prototype.hello = function() {
+  console.log('hello(上書きしたパターン) ' + this.name);
+}
+
+// Japaneseのプロトタイプのみメソッドを追加
+Japanese.prototype.bye = function() {
+  console.log('bye ' + this.name); // 継承元のPersonにはhelloしかない。
+}
+
+const taro = new Japanese('Taro', 23, 'Male');
+console.log(taro); // Japanese {name: 'Taro', age: 23}
+
+taro.hello(); // hello Taro
+taro.bye(); // bye Taro
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * コピーして流用する場合。記述が長くなる、修正しにくい。
+ * function Person(name, age) {
+  this.name = name;
+  this.age = age;
+}
+
+Person.prototype.hello = function() {
+  console.log('hello ' + this.name);
+}
+ */
