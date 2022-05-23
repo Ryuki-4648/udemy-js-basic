b7dac06dd6398814871e3536a3343f632563ff2b
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-05-23 21:30:13 +0900

    クラス　コンストラクター関数を、クラス表記でかけるようにしたもの

--- 060_関数とオブジェクト２/090_クラス/start/main.js
+++ 060_関数とオブジェクト２/090_クラス/start/main.js
@@ -1,8 +1,40 @@
+/** 
+ * クラス
+ * 
+ * コンストラクター関数を、クラス表記でかけるようにしたもの
+ * すでにあるものを簡単に描けるようにしたもの　→　シンタックスシュガー　という
+*/
+
+
+// コンストラクターをクラスに書き換える
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  hello() {
+    console.log('hello ' + this.name);
+  }
+}
+
+// インスタンス化してオブジェクトを生成する（newでオブジェクト作成する）
+const bob = new Person('Bob', 23);
+console.log(bob);
+// インスタンス化されたものはオブジェクトとなる
+
+
+
+/*
+元の姿。裏側の仕組み。
+コンストラクター関数
 function Person(name, age) {
   this.name = name;
   this.age = age;
 }
 
+プロトタイプ（オブジェクトに存在する特別なプロパティー。コンストラクター関数と合わせて使用する）
 Person.prototype.hello = function() {
   console.log('hello ' + this.name);
 }
+*/
