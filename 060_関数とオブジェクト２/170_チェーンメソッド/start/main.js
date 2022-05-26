b5691cc5d824acf0e3d4efa736b5789ba4be1b9f
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-05-27 06:51:23 +0900

    チェーンメソッド　1つのインスタンスに対して連続した処理を行う

--- 060_関数とオブジェクト２/170_チェーンメソッド/start/main.js
+++ 060_関数とオブジェクト２/170_チェーンメソッド/start/main.js
@@ -1,3 +1,7 @@
+/** 
+ * チェーンメソッド
+*/
+
 class Person {
 	constructor(name, age) {
 		this.name = name;
@@ -5,15 +9,19 @@ class Person {
 	}
 
 	hello(person) {
-		console.log(`${this.name} says hello ${person.name}`);
+    console.log(`${this.name} says hello ${person.name}`);
+    return this; // Personのインスタンス(newで作成されたオブジェクト)を返す。bobの場合はbobのオブジェクト。
+    // thisが参照するのはコンストラクター関数から作成したオブジェクトのインスタンスになる。
 	}
 
 	introduce() {
-		console.log(`Hi, I'm ${this.name}, ${this.age} years old.`);
+    console.log(`Hi, I'm ${this.name}, ${this.age} years old.`);
+    return this;
 	}
 
 	shakeHands(person) {
-		console.log(`${this.name} shake hands with ${person.name}.`);
+    console.log(`${this.name} shake hands with ${person.name}.`);
+    return this;
 	}
 
 	bye(person) {
@@ -24,7 +32,22 @@ class Person {
 const bob = new Person('Bob', 23);
 const tim = new Person('Tim', 33);
 
+console.log(bob); // Person {name: 'Bob', age: 23}
+
 bob.hello(tim);
 bob.introduce();
 bob.shakeHands(tim);
-bob.bye(tim);;
+bob.bye(tim);
+
+
+
+
+// 1つのインスタンスに対して連続した処理を行う時にチェーンメソッドを使う
+console.log('----- チェーンメソッドで記述 -----');
+bob.hello(tim) // bob.hello(time)の実行結果がbobであれば、次につなげることができる
+  .introduce()
+  .shakeHands(tim)
+  .bye(tim);
+
+
+// bob.hello(tim)のメソッドが完了した時点での戻り値がPersonのインスタンスであればintoroduceメソッドを発見できる
