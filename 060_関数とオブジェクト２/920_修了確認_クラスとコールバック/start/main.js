948799ac9435d66b44942d486e0fcc37d9fb57f6
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-05-30 07:05:37 +0900

    修了確認　クラスとコールバック関数　回答

--- 060_関数とオブジェクト２/920_修了確認_クラスとコールバック/start/main.js
+++ 060_関数とオブジェクト２/920_修了確認_クラスとコールバック/start/main.js
@@ -15,18 +15,44 @@ class Person {
   }
 
   hello() {
-    console.log('hello ' + this.name);
+    console.log(this); // windowオブジェクト　
+    console.log('hello ' + this.name); // windowオブジェクトのnameプロパティをhelloのあとに探しにいくが、見つからない
   }
 }
 
 const bob = new Person('Bob', 23);
 const call = bob.hello();
-setTimeout(call, 1000);
+setTimeout(call, 1000); // 結果1秒後にhelloだけ出力される
 
-console.log(bob); // Person {name: 'Bob', age: 23}
-console.log(bob.hello);
+console.log(bob); // Person {name: 'Bob', age: 23}　→　prototypeの中にhello: ƒ hello()
+console.log(bob.hello); // helloが参照している先の関数が実行されることになり、オブジェクトからのメソッドの呼び出しとはならない。
 /**
  *  ƒ hello(name) {
       console.log('hello ' + this.name);
     }
- */
+*/
+
+
+
+// 回答1
+class Person2 {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  hello() {
+    console.log('hello ' + this.name);
+  }
+}
+
+const bob2 = new Person2('Bob', 23);
+setTimeout(bob2.hello.bind(bob), 1000); // bobをhello内のthisとして束縛する
+
+// 回答2
+setTimeout(function(){
+  bob.hello(); // bobオブジェクトから呼び出されたhelloメソッドが実行されていることになる
+}, 1000);
+
+
+console.log(bob.hello === Person.prototype.hello); // true
