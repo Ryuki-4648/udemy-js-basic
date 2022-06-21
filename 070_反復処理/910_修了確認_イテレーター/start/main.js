2cf8df8d6a7ad652203498bc529d2a0c834dc748
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-06-22 07:53:30 +0900

    修了確認 イテレーター　自分の回答

--- 070_反復処理/910_修了確認_イテレーター/start/main.js
+++ 070_反復処理/910_修了確認_イテレーター/start/main.js
@@ -19,14 +19,36 @@
  * }
  * 
  * の値が順番にコンソールに表示されます。
- */
+*/
 
 
- 
-// const it = genStep(4, 10, 2);
-// let a = it.next();
+function genStep(min = 4, max = 10, step = 2){
+  let i = 0;
+  return {
+    next: function(){
+      if(i > max) {
+        return {
+          done: true
+        }
+      } else if (i < min) {
+        return {
+          done: true
+        }
+      } else {
+        return {
+          done: false,
+          value: i++
+        }
+      }
+    }
+  }
+}
 
-// while(!a.done) {
-//   console.log(a.value);
-//   a = it.next();
-// }
+
+const it = genStep(4, 10, 2);
+let a = it.next();
+
+while(!a.done) {
+console.log(a.value);
+a = it.next();
+}
