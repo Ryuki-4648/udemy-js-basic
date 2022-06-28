70524bdbfd3ad6be69819f9f88a08fb91592e7a0
yuki <72589895+Ryuki-4648@users.noreply.github.com>
2022-06-29 08:11:09 +0900

    タスクキューとコールスタック　キューに入ってきたものから順に処理

--- 080_非同期処理/020_タスクキューとコールスタック/start/main.js
+++ 080_非同期処理/020_タスクキューとコールスタック/start/main.js
@@ -1,15 +1,44 @@
+/**  
+ * タスクキューとコールスタック
+ * 
+ * ◎タスクキュー
+ * 実行待ちの非同期処理の行列
+ * 非同期処理の実行順を管理。
+ * 
+ * キューの仕組みを先入れ先出し＝FIFO（First IN, First Out）という。
+ * キューに入ってきたものから順に処理する。
+*/
+
+
 const btn = document.querySelector('button');
 btn.addEventListener('click', function task2() {
-    console.log('task2 done');
+  console.log('task2 done');
 });
 
 function a() {
-    setTimeout(function task1() {
-        console.log('task1 done');
-    }, 4000);
+  setTimeout(function task1() {
+    console.log('task1 done');
+  }, 4000);
 
-    const startTime = new Date();
-    while (new Date() - startTime < 5000);
+  const startTime = new Date();
+  while (new Date() - startTime < 5000); // 2秒間メインスレッドを占有する処理
 
-    console.log('fn a done');
+  console.log('fn a done');
 }
+
+
+a();
+
+/**
+ * 画面更新中にボタンをクリック
+ * 
+ * fn a done
+  task2 done
+  task1 done
+  の順番で表示される
+
+  タスクキューに入った処理（タスク）が非同期で実行される。
+
+  メインスレッドが占有されている状態　＝　コールスタックにコンテキストが積まれている状態！！！！！！
+  コールスタックのコンテキストがはけるまでは、タスクキューに入っているタスクは処理されない！！！！！
+*/
